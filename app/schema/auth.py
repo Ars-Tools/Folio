581e8a05a1436f7a46a46701efd06444283f8852
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-02-20 09:40:44 +0900

    remove schema/ directory (merged into models/)

--- app/schema/auth.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-class AuthRequest(BaseModel):
-    user: str = Field(..., description="The username for authentication")
-    password: str = Field(..., description="The password for the user")
-
